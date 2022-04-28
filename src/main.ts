0044ffc59066fa10bc750bf1786c8517436e8b53
Quill Liang <lhongquan.1998@gmail.com>
2022-04-28 15:03:08 +0700

    env

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(4444);
+  await app.listen(process.env.PORT || 4444);
 }
 bootstrap();
