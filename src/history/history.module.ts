fef9ca51ed9ee1e975dbe0fcd785877be7459f50
Quill Liang <lhongquan.1998@gmail.com>
2022-05-11 20:30:22 +0700

    audit api

--- src/history/history.module.ts
+++ src/history/history.module.ts
@@ -1,9 +1,11 @@
+import { RepositoryModule } from '@/repository';
 import { Module } from '@nestjs/common';
 import { HistoryService } from './service';
 
 const providers = [HistoryService];
 
 @Module({
+  imports: [RepositoryModule],
   providers,
   exports: providers,
 })
